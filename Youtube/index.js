e2aa4150a02797c0153527cd435b745c052ac978
Mallikarjun C V <mallikarjuncv12@gmail.com>
2024-10-04 22:49:08 +0530

    first commit

--- Youtube/index.js
+++ Youtube/index.js
@@ -3,9 +3,6 @@ var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
 var like1 = document.querySelector(".like1");
 var like2 = document.querySelector(".like2");
-var light = document.querySelector(".light");
-var header = document.querySelector(".header");
-var sideVideo = document.querySelector(".side-video");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
@@ -20,14 +17,3 @@ like1.onclick = function(){
 like2.onclick = function(){
     like2.classList.toggle("liked1");
 }
-
-light.onclick = function(){
-    header.classList.toggle("dark1")
-}
-
-sideVideo.onclick = function(){
-    sideVideo.classList.toggle("S1")
-}
-
-
-
